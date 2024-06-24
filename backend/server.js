218d9a2e0b727a3bf5a30cdc8744ac61aa5fa72c
EphiusJB <cloudjoseph797@gmail.com>
2024-06-25 01:49:29 +0200

    added routes on the server

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import router from './data.routes.js';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/v1/todo");
+app.use("/v1/todo", router);
 app.use("*",(req,res)=>{
     res.status(404).json({error: "not found"})
 });
